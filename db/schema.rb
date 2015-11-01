35b43b45d15bf55588ab4e5368db198f71817496
Masashi Ogawa <ogawamasashi13@gmail.com>
2015-11-01 23:13:28 +0900

    会員登録できた！

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101085112) do
+ActiveRecord::Schema.define(version: 20151101141033) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20151101085112) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "user_name",              limit: 255
+    t.string   "member",                 limit: 255
+    t.string   "works",                  limit: 255
+    t.string   "profile",                limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
