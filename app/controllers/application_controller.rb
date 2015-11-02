016451aae501ff08dc4e9b423bfb760b374f3b87
Masashi Ogawa <ogawamasashi13@gmail.com>
2015-11-03 01:32:02 +0900

    ログイン成功時に表示を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up)        << [:user_name, :avatar, :member, :profile, :works]
     devise_parameter_sanitizer.for(:sign_in)        << [:user_name, :avatar, :member, :profile, :works]
     devise_parameter_sanitizer.for(:account_update) << [:user_name, :avatar, :member, :profile, :works]
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
   end
 end
