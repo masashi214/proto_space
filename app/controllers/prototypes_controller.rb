5ceaa2638b746532ab9af6eb7eb0aa5456e7179c
Masashi Ogawa <ogawamasashi13@gmail.com>
2015-11-03 05:14:17 +0900

    ユーザー関連修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,7 @@ class PrototypesController < ApplicationController
   def index
 
   end
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.update(user_edit_param)
+    redirect_to action: :show
+  end
+
+  private
+  def user_edit_param
+    params.require(:user).permit(:user_name, :avatar, :email, :password, :member, :profile, :works)
+  end
 end
