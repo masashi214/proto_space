fe0271443be7731844960634af5f9e84b764b8fd
Masashi Ogawa <ogawamasashi13@gmail.com>
2015-11-03 05:55:44 +0900

    モデル修正

--- db/migrate/20151102204253_create_prototypes.rb
+++ db/migrate/20151102204253_create_prototypes.rb
@@ -1,6 +1,11 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
+      t.string     :title
+      t.string     :catch_copy
+      t.string     :concept
+      t.integer    :total_likes
+      t.references :user
 
       t.timestamps null: false
     end
