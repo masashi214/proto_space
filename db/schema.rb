03e7ab0fa191bc9265d1dce8f1b90421221859dd
Masashi Ogawa <ogawamasashi13@gmail.com>
2015-11-14 15:31:20 +0900

    typeカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102204749) do
+ActiveRecord::Schema.define(version: 20151107081708) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "url",          limit: 255
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "type",         limit: 1
   end
 
   create_table "prototypes", force: :cascade do |t|
